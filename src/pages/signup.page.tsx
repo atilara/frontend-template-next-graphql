26bfda6fd680e1a7c35b43fe1fb41d05ea826a90
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-02-24 11:05:21 -0300

    feat: create signup page

---
+++ src/pages/signup.page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Signup() {
+  return (
+    <div>
+      Welcome to the signup page. Go to the <Link href="/">Home</Link> page.
+    </div>
+  );
+}
