3df332739cc550a6c580afaf84f9de41b8022a8e
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-02-24 11:41:53 -0300

    feat: add loading effect to submit btn

--- src/pages/signin.page.tsx
+++ src/pages/signin.page.tsx
@@ -1,59 +1,75 @@
 import { FormEvent, useCallback, useState } from 'react';
 import { useAuth } from '@/hooks/auth';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input, Row } from 'antd';
 
 export default function Signin() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [form] = Form.useForm();
+  const [loadings, setLoadings] = useState<boolean[]>([]);
 
   const { signIn } = useAuth();
 
   const onFinishhandleSubmit = useCallback(
     async (values: any) => {
-
-      console.log(values)
+      setLoadings([ true ]);
       
-      await signIn(values);
+      console.log(values);
+      console.log(loadings);
+
+      setTimeout(async () => {
+        await signIn(values);
+        
+        setLoadings([ false ]);
+      }, 3000);
+      
     },
     [signIn]
   );
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
+    setLoadings((prevLoadings) => {
+      const newLoadings = [...prevLoadings];
+      newLoadings[0] = false;
+      return newLoadings;
+    });
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinishhandleSubmit}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      form={form}
+    <Row
+      justify="center"
+      align="middle"
+      style={{ minHeight: '100vh' }}
     >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinishhandleSubmit}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        form={form}
       >
-        <Input/>
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password/>
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item style={{ float: 'right' }}>
+          <Button loading={loadings[0]} type="primary" htmlType="submit" >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Row>
   );
 }
